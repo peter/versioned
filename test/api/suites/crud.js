18a7031fc3ed030419c676daf9445f4a0e50f7d7
Peter Marklund <peter@marklunds.com>
2016-07-22 22:16:18 +0200

    Completed crud api test

--- test/api/suites/crud.js
+++ test/api/suites/crud.js
@@ -2,6 +2,15 @@
 
 var assert = require('assert');
 
+var page = {
+  "title": {
+    "se": "Testsida"
+  },
+  "slug": {
+    "se": "testsida"
+  }
+};
+
 module.exports = {
   suite: {
     name: "CRUD",
@@ -13,57 +22,73 @@ module.exports = {
           {
             it: "can create a page document",
             request: "POST /pages",
-            params: {pages: "{{pages.start}}"},
+            params: {pages: page},
             headers: "{{headers.admin}}",
-            // status: 201,
-            // assert: function(body) {
-            //   assert(body.createdAt);
-            //   assert(new Date(body.createdAt) > new Date());
-            //   this.pages.start.id = body.id; // save the ID
-            // }
+            assert: {
+              select: "body.data",
+              equal_keys: {
+                title: page.title
+              }
+            },
+            save: {
+              "page_id": "body.data.id"
+            }
           },
-          // {
-          //   it: "can get the created page document",
-          //   request: "GET /pages/{{pages.start.id}}",
-          //   assert: function(body) {
-          //     assert.equal(body.playerName, this.pages.start.playerName);
-          //   }
-          // },
-          // {
-          //   it: "can list page documents",
-          //   request: "GET /pages",
-          //   params: {
-          //     order: "-created_at",
-          //     limit: 3
-          //   },
-          //   assert: function(body) {
-          //     assert.equal(body.results.length, 3);
-          //     assert.equal(body.results[0].id, this.pages.start.id);
-          //   }
-          // },
-          // {
-          //   it: "can update name of page document",
-          //   request: "PUT /pages/{{pages.start.id}}",
-          //   params: {
-          //     playerName: "New name"
-          //   }
-          // },
-          // {
-          //   it: "can get new name of page document",
-          //   request: "GET /pages/{{pages.start.id}}",
-          //   assert: function(body) {
-          //     assert.equal(body.playerName, "New name");
-          //   }
-          // },
-          // {
-          //   it: "can delete page document",
-          //   request: "DELETE /pages/{{pages.start.id}}"
-          // },
-          // {
-          //   it: "can verify that page document was deleted",
-          //   request: "GET /pages/{{pages.start.id}}",
-          //   status: 404
-          // }
+          {
+            it: "can get the created page document",
+            request: "GET /pages/{{page_id}}",
+            headers: "{{headers.admin}}",
+            assert: {
+              select: "body.data.attributes",
+              equal_keys: {
+                id: "{{page_id}}",
+                title: page.title
+              }
+            }
+          },
+          {
+            it: "can list page documents",
+            request: "GET /pages",
+            headers: "{{headers.admin}}",
+            assert: {
+              select: "body.data",
+              contains_keys: {
+                id: "{{page_id}}",
+                title: page.title
+              }
+            }
+          },
+          {
+            it: "can update name of page document",
+            request: "PUT /pages/{{page_id}}",
+            headers: "{{headers.admin}}",
+            params: {
+              pages: {title: {se: "Testsida EDIT"}}
+            }
+          },
+          {
+            it: "can get new name of page document",
+            request: "GET /pages/{{page_id}}",
+            headers: "{{headers.admin}}",
+            assert: {
+              select: "body.data.attributes",
+              equal_keys: {
+                id: "{{page_id}}",
+                title: {se: "Testsida EDIT"}
+              }
+            }
+          },
+          {
+            it: "can delete page document",
+            request: "DELETE /pages/{{page_id}}",
+            headers: "{{headers.admin}}",
+          },
+          {
+            it: "can verify that page document was deleted",
+            request: "GET /pages/{{page_id}}",
+            headers: "{{headers.admin}}",
+            status: 404
+          }
         ]
       }
     ]
