15ad5729da2af8659efe0ad64a7af98e40e7d275
Peter Marklund <peter@marklunds.com>
2017-01-16 13:55:59 +0100

    Fixed model-spec/generate-spec function so that it works when callbacks is nil (happened when running example app). Bumped version 0.11.17

--- src/versioned/model_spec.clj
+++ src/versioned/model_spec.clj
@@ -106,7 +106,11 @@
   [& specs :- [Coll]]
   (let [specs (flatten specs)
         schema (apply merge-schemas (u/compact (map :schema specs)))
-        callbacks (not-empty (sort-callbacks (apply merge-callbacks (map normalize-callbacks (u/compact (map :callbacks specs))))))
+        callbacks (some->> (map :callbacks specs)
+                           (u/compact)
+                           (not-empty)
+                           (map normalize-callbacks)
+                           (apply merge-callbacks))
         relationships (apply u/deep-merge (u/compact (map normalized-relationships specs)))
         indexes (flatten (u/compact (map :indexes specs)))
         merged-spec (apply merge specs)
